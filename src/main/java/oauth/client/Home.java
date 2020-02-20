65449c17002ff1699074238faf086328ecdf4c36
Kleber Mota de Oliveira <klebermo@gmail.com>
2020-02-20 11:02:00 -0300

    Home.java

--- src/main/java/oauth/client/Home.java
+++ src/main/java/oauth/client/Home.java
@@ -2,16 +2,14 @@ package oauth.client;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
 
 @Controller
 public class Home {
   @RequestMapping("/")
   public String index() {
     return "index";
-  }
-
-  @RequestMapping(value="/login", method=RequestMethod.GET)
-  public String login(@RequestParam("code") String code) {
-    return "redirect:/";
   }
 }
