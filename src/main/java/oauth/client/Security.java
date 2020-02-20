528932595e1ec072435f885e600cd637e46eba69
Kleber Oliveira <klebermo@gmail.com>
2020-02-20 13:29:14 -0300

    Security.java

--- src/main/java/oauth/client/Security.java
+++ src/main/java/oauth/client/Security.java
@@ -9,6 +9,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.oauth2.client.web.HttpSessionOAuth2AuthorizationRequestRepository;
+import org.springframework.security.oauth2.client.endpoint.NimbusAuthorizationCodeTokenResponseClient;
 
 @Configuration
 @EnableWebSecurity
@@ -18,20 +20,25 @@ public class Security extends WebSecurityConfigurerAdapter {
   @Override
 	public void configure(HttpSecurity http) throws Exception {
 			http
-					.authorizeRequests(a -> a
+					.authorizeRequests()
 							.antMatchers("/", "/login" ,"/css/**", "/js/**", "/img/**").permitAll()
 							.anyRequest().authenticated()
-					)
-					.exceptionHandling(e -> e
+          .and()
+					.exceptionHandling()
 							.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
-					)
-					.csrf(c -> c
+          .and()
+					.csrf()
 	            .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-	        )
-					.logout(l -> l
+          .and()
+					.logout()
 							.logoutUrl("/logout")
 					    .logoutSuccessUrl("/").permitAll()
-					)
-					.oauth2Login();
+          .and()
+					.oauth2Login()
+              .authorizationEndpoint().authorizationRequestRepository(new HttpSessionOAuth2AuthorizationRequestRepository())
+              .and()
+              .redirectionEndpoint().baseUri("/login")
+              .and()
+              .tokenEndpoint().accessTokenResponseClient(new NimbusAuthorizationCodeTokenResponseClient());
 	}
 }
