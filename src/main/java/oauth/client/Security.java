6ec52e7ab6a3e55e18612d8481e0b15cbede883f
Kleber Mota de Oliveira <klebermo@gmail.com>
2020-02-21 08:20:13 -0300

    Security.java

--- src/main/java/oauth/client/Security.java
+++ src/main/java/oauth/client/Security.java
@@ -9,21 +9,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.security.oauth2.client.web.HttpSessionOAuth2AuthorizationRequestRepository;
-import org.springframework.security.oauth2.client.endpoint.NimbusAuthorizationCodeTokenResponseClient;
-import org.springframework.security.oauth2.client.endpoint.DefaultAuthorizationCodeTokenResponseClient;
-import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizationRequestResolver;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
-import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
-import org.springframework.security.oauth2.core.AuthorizationGrantType;
-import org.springframework.security.oauth2.core.AuthenticationMethod;
-import java.util.List;
-import java.util.ArrayList;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.oauth2.client.web.HttpSessionOAuth2AuthorizationRequestRepository;
-import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizationRequestResolver;
 
 @Configuration
 @EnableWebSecurity
@@ -47,36 +32,6 @@ public class Security extends WebSecurityConfigurerAdapter {
 							.logoutUrl("/logout")
 					    .logoutSuccessUrl("/").permitAll()
           .and()
-          .oauth2Client(c -> c
-              .authorizationCodeGrant()
-                  .authorizationRequestRepository(new HttpSessionOAuth2AuthorizationRequestRepository())
-                  .authorizationRequestResolver(new DefaultOAuth2AuthorizationRequestResolver(clientRegistrationRepository(), "http://localhost:8080/oauth/authorize"))
-          )
-					.oauth2Login(l -> l
-              .clientRegistrationRepository(clientRegistrationRepository())
-          );
+					.oauth2Login();
 	}
-
-  @Bean
-	public ClientRegistrationRepository clientRegistrationRepository() {
-  		List<ClientRegistration> registrations = new ArrayList<>();
-  		registrations.add(clientRegistration());
-  		return new InMemoryClientRegistrationRepository(registrations);
-	}
-
-  private ClientRegistration clientRegistration() {
-    return ClientRegistration.withRegistrationId("server")
-        .clientId("first-client")
-        .clientSecret("noonewilleverguess")
-        .clientAuthenticationMethod(ClientAuthenticationMethod.BASIC)
-        .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-        .scope("read")
-        .redirectUriTemplate("{baseUrl}/")
-        .authorizationUri("http://localhost:8080/oauth/authorize")
-        .tokenUri("http://localhost:8080/oauth2/check_token")
-        .userInfoUri("http://localhost:8080/oauth2/userinfo")
-        .jwkSetUri("http://localhost:8080/.well-known/jwks.json")
-        .userInfoAuthenticationMethod(AuthenticationMethod.FORM)
-        .clientName("server").build();
-  }
 }
